2b0535a5a5d4f67f468db675fe64f20df459600c
NicolaspSousa <61783307+NicolaspSousa@users.noreply.github.com>
2022-06-09 15:46:00 +0100

    Versão com SearchBar

--- routes/books.js
+++ routes/books.js
@@ -1,78 +1,99 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models/Book');
+
+
 // const VerificarAutenticacao = require('../middleware/VerificarAutenticacao');
 const VerificarAutorizacao = require('../middleware/VerificarAutorizacao');
+const { application } = require('express');
 
-router.get('/',  async(req, res) => {
-    const books = await Book.find({});
-    res.render('books/home', {books: books, user: req.user});
+router.get('/', async (req, res) => {
+
+    if (req.query.search) {
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+
+        const books = await Book.find({ title: regex });
+        res.render('books/home', { books: books, user: req.user });
+
+
+
+
+
+
+    } else {
+        const books = await Book.find({});
+        res.render('books/home', { books: books, user: req.user});
+    }
+
 });
 
 
 
-router.get('/view/:id', (req, res)=>{
-    Book.findById(req.params.id).populate('comments').exec((err, book)=>{
-        if(err){
+router.get('/view/:id', (req, res) => {
+    Book.findById(req.params.id).populate('comments').exec((err, book) => {
+        if (err) {
             console.log(err);
             res.redirect('/books');
-        }else{
-            res.render('books/view', {book: book, user: req.user});
+        } else {
+            res.render('books/view', { book: book, user: req.user });
         }
     });
 });
 
-router.get('/new', VerificarAutorizacao ,(req, res) => {
+router.get('/new', VerificarAutorizacao, (req, res) => {
     res.render('books/new');
 });
 
-router.post('/new', VerificarAutorizacao ,async (req, res) => {
+router.post('/new', VerificarAutorizacao, async (req, res) => {
     const book = {
         title: req.body.title,
         image: req.body.image,
         description: req.body.description,
-        price:  parseFloat(req.body.price),
+        price: parseFloat(req.body.price),
         stock: req.body.stock,
         category: req.body.category,
     };
-    try{
+    try {
         const newBook = new Book(book);
         await newBook.save();
         res.redirect(`/books/view/${newBook._id}`);
-    }catch(e){
+    } catch (e) {
         console.log(e);
-        res.render('books/new', {book: book});
+        res.render('books/new', { book: book });
     }
 });
 
-router.get('/edit/:id', VerificarAutorizacao ,async (req, res)=>{
+router.get('/edit/:id', VerificarAutorizacao, async (req, res) => {
     const book = await Book.findById(req.params.id);
-    res.render('books/edit', {book: book});
+    res.render('books/edit', { book: book });
 });
-router.put('/edit/:id', VerificarAutorizacao ,async (req, res)=>{
+router.put('/edit/:id', VerificarAutorizacao, async (req, res) => {
     const book = {
         title: req.body.title,
         image: req.body.image,
         description: req.body.description,
-        price:  parseFloat(req.body.price),
+        price: parseFloat(req.body.price),
         stock: req.body.stock,
         category: req.body.category
     };
-    try{
+    try {
         const updatedBook = await Book.findByIdAndUpdate(req.params.id, book);
         await updatedBook.save();
         res.redirect(`/books/view/${updatedBook._id}`);
-    }catch(e){
+    } catch (e) {
         console.log(e);
-        res.render('books/new', {book: book});
+        res.render('books/new', { book: book });
     }
 });
 
-router.delete('/delete/:id', VerificarAutorizacao ,async (req, res) => {
+router.delete('/delete/:id', VerificarAutorizacao, async (req, res) => {
     await Book.findByIdAndDelete(req.params.id);
     res.redirect('/books');
 });
 
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
 
 
 module.exports = router;
